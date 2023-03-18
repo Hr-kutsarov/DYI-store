098d95f0b8ba0b2c720500fe0e801e7e2ab8e6cf
Hristo Kutsarov <103206944+Hr-kutsarov@users.noreply.github.com>
2023-03-18 18:18:41 +0200

    Altered Product model to display price with decimal places. Added Product type field.

--- api/models.py
+++ api/models.py
@@ -18,8 +18,9 @@ class Section(models.Model):
 #  location, picture, is_promoted, is_discounted, is_new
 class Product(models.Model):
     title = models.CharField(max_length=60, blank=False, null=False, unique=True)
+    type = models.CharField(max_length=60, blank=False, null=False, default='product')
     thumbnail = models.ImageField(upload_to='thumbnail/images', blank=True, null=True)
-    price = models.IntegerField(blank=False, null=False)
+    price = models.DecimalField(max_digits=15, decimal_places=2, blank=False, null=False)
     quantity = models.IntegerField(blank=False, null=False)
     description = models.CharField(max_length=600, blank=True, null=True)
     location = models.ForeignKey(Store, on_delete=models.CASCADE)

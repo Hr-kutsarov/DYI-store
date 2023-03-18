65d6ccc5cc23f83ef936dea501f8e44dd905c959
Hristo Kutsarov <103206944+Hr-kutsarov@users.noreply.github.com>
2023-03-18 16:34:07 +0200

    Fixed issue with 'cors-headers'.

--- src/settings.py
+++ src/settings.py
@@ -16,10 +16,10 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:8080/",
-    "http://localhost:8000/",
-    "http://localhost:3000/",
-    "http://127.0.0.1:3000/",
+    "http://localhost:8080",
+    "http://localhost:8000",
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
 ]
 
 
